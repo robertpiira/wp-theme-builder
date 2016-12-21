c5a4b28de95f9a1eea98255713e296d74fbd37c4
robertpiira <robert@piira.se>
2016-12-21 23:46:45 +0100

    remove that pesky admin bar

--- src/functions.php
+++ src/functions.php
@@ -27,6 +27,9 @@ function exampletheme_setup() {
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
+  // Remove that pesky admin bar
+  add_filter('show_admin_bar', '__return_false');
+
 	/*
 	 * Let WordPress manage the document title.
 	 * By adding theme support, we declare that this theme does not use a
